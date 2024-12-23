80697cc2c5bf03bd14d82a7430e86868509f53b9
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2024-12-23 10:08:11 +0500

    update welcome message to indicate API status

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Welcome");
+  res.send("API is running");
 });
 
 app.use("/api/categories", categoryRoutes);
